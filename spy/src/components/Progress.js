e2b38cb9d3525f9789977d45546e2de3849f3ce5
Maranatha <67089614+emxhive@users.noreply.github.com>
2023-07-03 22:33:47 +0100

    Perfect

--- spy/src/components/Progress.js
+++ spy/src/components/Progress.js
@@ -1,36 +1,43 @@
 import PropTypes from "prop-types";
+import styledComp from 'styled-components'
 
 function Progress({ pmcolor, percent, pmicon }) {
+
+  const Div = styledComp.div`
+  background: conic-gradient(${pmcolor} ${(percent+'%')}, white 0);
+  `
   const styles = {
     outerC: {
-      width: "70px",
+  
+      width: "48px",
       aspectRatio: 1,
       borderRadius: "50%",
       display: "inline-grid",
       placeContent: "center",
-      backgroundColor: pmcolor,
+     
     },
     innerC: {
-      width: "50px",
+      width: "31px",
       aspectRatio: 1,
       borderRadius: "50%",
       display: "inline-grid",
       placeContent: "center",
-      backgroundColor: 'white'
+      backgroundColor: "white",
     },
 
     icon: {
       aspectRatio: 1,
-      width: '30px'
+      width: "29px",
+      borderRadius: '50%',
     },
   };
 
   return (
-    <div style={styles.outerC}>
+    <Div style={styles.outerC}>
       <div style={styles.innerC}>
-        <img src={pmicon} style={styles.icon} alt={percent+ '%'} />
-      </div>
-    </div>
+        <img src={pmicon} style={styles.icon} alt={percent + "%"} />{" "}
+      </div>{" "}
+    </Div>
   );
 }
 
